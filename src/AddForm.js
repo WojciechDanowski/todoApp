b7c6cff9ab5cdffadc85fd917ffaa1cb5588a883
Wojciech Danowski <wojciechdanowskiz@gmail.com>
2019-11-26 21:23:35 +0100

    Added a new component file with basic function that should read inputed value

---
+++ src/AddForm.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class AddTodo extends Component {
+  state = {
+    content: ""
+  };
+  handleChange = e => {
+    this.setState({
+      content: e.target.value
+    });
+  };
+
+  render() {
+    <div>
+      <form onSubmit={}>
+        <label>Add new todo: ...</label>
+        <input type="text" onChange={this.handleChange} />
+      </form>
+    </div>;
+  }
+}
